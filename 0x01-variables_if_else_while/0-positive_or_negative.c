8a5ea5ff42dcf6bfcf4afd7dd424da2e7d81e650
“Owen <magazine.Owen@gmail.com>
2022-09-09 14:48:03 -0700

    if-else-while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,6 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;	
 	if (n > 0)
@@ -18,6 +17,5 @@ int main(void)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-
 	return (0);
 }
