f1dbedf5c32df5f5d3baf48570c1a24ac032c754
“Owen <magazine.Owen@gmail.com>
2022-09-09 15:24:26 -0700

    if-else-while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,7 +9,7 @@
 */
 
 int main(void)
-{	
+{
 	int d, p, q;
 
 	for (d = '0'; d < '9'; d++)
@@ -23,10 +23,10 @@ int main(void)
 					putchar(d);
 					putchar(p);
 					putchar(q);
-					
+
 					if (d == '7' && p == '8')
 						continue;
-					
+
 					putchar(',');
 					putchar(' ');
 				}
